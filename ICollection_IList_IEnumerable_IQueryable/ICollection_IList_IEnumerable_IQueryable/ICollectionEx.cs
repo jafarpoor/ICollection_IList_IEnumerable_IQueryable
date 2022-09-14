b2499afc055136ae6c63bf6320ea2bd57425aee2
جعفرپورلشکامی - فائزه <jafarpoor-f@tehran.iri>
2022-09-14 15:43:38 +0430

    example

---
+++ ICollection_IList_IEnumerable_IQueryable/ICollection_IList_IEnumerable_IQueryable/ICollectionEx.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace ICollection_IList_IEnumerable_IQueryable
+{
+    //ICollection
+    //Collection<T>
+    //IEnumerable is that ICollection is a write operation and IEnumerable is a read-only operation only meant for iteration
+    internal class ICollectionEx
+    {
+        public static void CheckCollection()
+        {
+            List<string> monthsofYear = new List<string>
+            {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
+
+            //casted to ICollection    
+            ICollection<string> collection = monthsofYear;
+            Console.WriteLine(collection.Count);
+        } 
+    }
+}
---
+++ ICollection_IList_IEnumerable_IQueryable/ICollection_IList_IEnumerable_IQueryable/IEnumerableEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICollection_IList_IEnumerable_IQueryable
+{
+    internal class IEnumerableEx
+    {
+
+        //MyDataContext dc = new MyDataContext();
+        //IEnumerable<Employee> list = dc.Employees.Where(p => p.Name.StartsWith("S"));
+        //list = list.Take<Employee>(10);
+
+
+           //  SELECT  [t0].[EmpID], [t0].[EmpName], [t0].[Salary]
+           // FROM[Employee] AS[t0]
+           //WHERE[t0].[EmpName]
+           // LIKE @p0
+
+
+        //Top 10 excute in client side
+    }
+
+   
+}
